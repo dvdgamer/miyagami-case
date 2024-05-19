39220789045b433e5356e9261182fc8b3ebadda7
dvdgamer <davidnmponte@gmail.com>
2024-05-19 18:21:14 +0200

    Added basic styling to the cards

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
+// import styled from "@emotion/styled";
+
 
 export default function Home() {
   const [data, setData] = useState(null);
+
 
   // Fetches data from the express server
   useEffect(() => {
@@ -11,10 +14,9 @@ export default function Home() {
       .then((data) => setData(data));
   }, []);
 
-  //data.feed.entry[item].
 
-  // console.log("data :", data);
 
+  // displays Loading while the components are loading
   if (!data) {
     return <div>Loading...</div>;
   }
@@ -22,21 +24,25 @@ export default function Home() {
 
   return (
     <main>
-      {/* generates cards from API */}
-      {data && data.feed.entry.map((item: any) => {
-        console.log("item: ", item);
-        console.log("item: ", item.link[1].$.href);
-        return (
-          <div key={item.id}>
-            <p>{item.title}</p>
-            <Image
-              src={item.link[1].$.href}
-              alt={item.title}
-              width={100}
-              height={100}/>
-          </div>
-        );
-      })}
+      <div className="container">
+
+        {/* fetches name and image for each card */}
+        {data && data.feed.entry.map((item: any) => {
+          return (
+            <div key={item.id}
+            className="p-4 m-4"
+            class="cards-container">
+              <p>{item.title}</p>
+              <Image
+                src={item.link[1].$.href}
+                alt={item.title}
+                width={500}
+                height={500}/>
+            </div>
+          );
+        })}
+
+      </div>
     </main>
   );
 }
